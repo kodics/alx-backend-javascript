b1beb74fc4171665070e011c00b8296378c59050
kodics <kodicsworld@gmail.com>
2024-03-08 18:35:21 -0800

    ES6_promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // Perform asynchronous operations here (if needed)
+
+    // For simplicity, let's resolve the Promise immediately
+    resolve('API response data');
+  });
+}
+
+export default getResponseFromAPI;
